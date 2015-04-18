6ee802d442c097e98fe79ed03dcbfb3553dce9ed
evilryu <osman.sist@gmail.com>
2015-04-18 12:40:57 -0600

    added condescending wonka meme, more manners to trollbot

--- src/scripts/slackTroll.ts
+++ src/scripts/slackTroll.ts
@@ -26,9 +26,19 @@ class SlackTroll {
 				msg.reply(memeURL);
 			});
 		});
+
+		robot.hear(/what an interesting link/i, (msg: any) => {
+			var promise = this.promise("http://version1.api.memegenerator.net/Instance_Create?username=matador&password=matador&languageCode=en&generatorID=45&imageID=9488555&text0=So youve decided to share a link&text1=Please tell us more about it");
+
+			promise.then((body) => {
+				var memeData = JSON.parse(body);
+				var memeURL = memeData.result.instanceImageUrl;
+				msg.reply(memeURL);
+			});
+		});
 	}
 }
 
 var httpClient = require("request-promise");
-var fn = new SlackTroll(httpClient).trollAction
+var fn = new SlackTroll(httpClient).trollAction;
 export = fn
